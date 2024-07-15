b4cea1eb6be9d68d73ec2d3568d67b28420aee7b
Luuan11 <luancraft55@gmail.com>
2024-07-14 23:43:41 -0300

    :sparkles: feat: implementing the home project

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import styles from "./Footer.module.css";
+
+function Footer(){
+    return (
+        <footer className={styles.styles}>
+            <h2>Made by Luan</h2>
+        </footer>
+    )
+}
+export default Footer
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,12 @@
+import Header from "components/Header";
+import Footer from "components/Footer";
+
 function Home(){
     return (
-        <h1>Hello</h1>
+        <>
+            <Header />
+            <Footer />
+        </>
     )
 }
 export default Home;
