76002a4013bc259ec1a30f2020d0dec1b21bc5e8
Luuan11 <luancraft55@gmail.com>
2024-07-16 23:48:44 -0300

    :sparkles: feat: implementing the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import styles from "./Footer.module.css";
 
 function Footer(){
     return (
-        <footer className={styles.styles}>
+        <footer className={styles.footer}>
             <h2>Made by Luan</h2>
         </footer>
     )
