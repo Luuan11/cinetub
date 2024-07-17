fe08050cd97a44daf744ae1f160bf4df3784571e
Luuan11 <luancraft55@gmail.com>
2024-07-16 23:49:14 -0300

    :sparkles: feat: implementing the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 import logo from "./logo.png";
-import styles from "./Header.module.css";
 import HeaderLink from "components/HeaderLink";
+
+import styles from "./Header.module.css";
 
 function Header() {
     return (
