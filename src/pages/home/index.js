f27cb099c1fe155e719a42ca79c0259fa90979ba
Luuan11 <luancraft55@gmail.com>
2024-07-17 22:06:20 -0300

    :sparkles: feat: implementing the title and using in the home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,16 @@
 import Header from "components/Header";
 import Footer from "components/Footer";
 import Banner from "components/Banner";
+import Title from "components/Title";
 
 function Home(){
     return (
         <>
             <Header />
             <Banner image="home" />
+            <Title>
+                <h1>Local perfeito para seus videos especiais!</h1>
+            </Title>
             <Footer />
         </>
     )
