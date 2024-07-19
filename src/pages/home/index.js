4755fec8ba8923b4494351c65d95431d005ecf55
Luuan11 <luancraft55@gmail.com>
2024-07-18 22:59:23 -0300

    :sparkles: feat: reusing components

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,31 +1,24 @@
-import Header from "components/Header";
-import Footer from "components/Footer";
 import Banner from "components/Banner";
 import Title from "components/Title";
 import Cards from "components/Cards";
 
-import videos from 'json/db.json';
+import videos from "json/db.json";
 
-import styles from './Home.module.css'
+import styles from "./Home.module.css";
 
-function Home(){
-    return (
-        <>
-            <Header />
-            <Banner image="home" />
-            <Title>
-                <h1>Local perfeito para seus videos especiais!</h1>
-            </Title>
-            <section className={styles.videoContainer}>
-
-                {videos.map((video) => {
-                    return <Cards {...video} key={video.id} />
-                })}
-
-            </section>
-
-            <Footer />
-        </>
-    )
+function Home() {
+  return (
+    <>
+      <Banner image="home" />
+        <Title>
+            <h1>Local perfeito para seus videos especiais!</h1>
+        </Title>
+        <section className={styles.videoContainer}>
+            {videos.map((video) => {
+            return <Cards {...video} key={video.id} />;
+            })}
+        </section>
+    </>
+  );
 }
 export default Home;
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,23 @@
-import Home from "pages/home"
+import Container from "components/Container"
+import Footer from "components/Footer"
+import Header from "components/Header"
+
+import Favorites from "pages/Favorites"
+import Home from "pages/Home"
+
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function AppRoutes(){
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home/>}></Route>
-            </Routes>
+        <Header />
+            <Container>
+                <Routes>
+                    <Route path="/" element={<Home/>}></Route>
+                    <Route path="/favorites" element={<Favorites/>}></Route>
+                </Routes>
+            </Container>
+        <Footer />
         </BrowserRouter>
     )
 }
