f9496b8f1d19e64e158ce7f03d64713614a9852f
Luuan11 <luancraft55@gmail.com>
2024-07-22 22:49:12 -0300

    :sparkles: feat: implementing the favorite system

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,14 +1,22 @@
+import { useFavoriteContext } from 'Context/Favorite';
 import styles from './Card.module.css'
 import iconFavorite from './favorite.png'
+import iconDisfavorite from "./unfavorite.png"
 
-function Cards({id, title, capa}) {
-    return(
+function Cards({ id, title, capa }) {
+    const { favorite, addFavorite } = useFavoriteContext();
+    const isFavorite = favorite.some((fav) => fav.id === id)
+    const icon = !isFavorite ? iconFavorite : iconDisfavorite;
+    return (
         <div className={styles.card}>
             <img src={capa} alt={title} className={styles.capa}/>
             <h2>{title}</h2>
-            <img src={iconFavorite} 
+            <img src={icon} 
                  alt="Favorite video" 
-                 className={styles.favorite}/>
+                 className={styles.favorite}
+                 onClick={() => {
+                    addFavorite({ id, title, capa })
+                 }}/>
         </div>
     )
 }
