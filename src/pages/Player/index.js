9cd6091b56ebb1a65870a7b42ce2cbddfe109b68
Luuan11 <luancraft55@gmail.com>
2024-07-23 23:58:54 -0300

    :sparkles: feat: implementing page Player

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -1,9 +1,18 @@
-import styles from "./Player.module.css"
+import Banner from "components/Banner";
+import styles from "./Player.module.css";
+import Title from "components/Title";
 
-function Player(){
-    return (
-        <h1>Player</h1>
-    )
+function Player() {
+  return (
+    <>
+      <Banner image="player" />
+        <Title>
+            <h1 className={styles.h1}> 
+                Player
+            </h1>
+        </Title>
+    </>
+  );
 }
 
 export default Player;
