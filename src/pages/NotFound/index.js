d1a9ae738961a1ffe53b3bff01ea71cf62a6f471
Luuan11 <luancraft55@gmail.com>
2024-07-25 22:32:07 -0300

    :lipstick: feat: change the style from not found page

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -3,8 +3,11 @@ import styles from "./NotFound.module.css"
 function NotFound(){
     return(
         <section className={styles.notFound}>
-            <h2>Hey Astronaut</h2>
-            <p>Are you lost ? </p>
+            <div className={styles.divnot}>
+                <h2>Hey Astronaut</h2>
+                <p>Are you lost ? </p>
+                <p>Page Not Found :C </p>
+            </div>
         </section>
     )
 }
