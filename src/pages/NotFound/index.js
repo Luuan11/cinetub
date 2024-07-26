2af78da45a471de137086e1a71e9de247e7c63a5
Luuan11 <luancraft55@gmail.com>
2024-07-25 22:01:04 -0300

    :sparkles: feat: new page

---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,12 @@
+import styles from "./NotFound.module.css"
+
+function NotFound(){
+    return(
+        <section className={styles.notFound}>
+            <h2>Hey Astronaut</h2>
+            <p>Are you lost ? </p>
+        </section>
+    )
+}
+
+export default NotFound;
