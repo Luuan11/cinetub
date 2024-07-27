76cbd154414ca7b00b6cd1d45cce08af59484dfa
Luuan11 <luancraft55@gmail.com>
2024-07-26 23:41:27 -0300

    :sparkles: feat: changing the routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,5 @@
-import Container from "components/Container";
-import Footer from "components/Footer";
-import Header from "components/Header";
-import FavoritesProvider from "Context/Favorite";
+
+import BasePage from "pages/BasePage";
 
 import Favorites from "pages/Favorites";
 import Home from "pages/Home";
@@ -13,20 +11,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 function AppRoutes() {
   return (
     <BrowserRouter>
-      <Header />
 
-      <Container>
-        <FavoritesProvider>
           <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/favorite" element={<Favorites />}></Route>
-            <Route path="/:id" element={<Player />}></Route>
-            <Route path="*" element={<NotFound />}></Route>
+            <Route path="/" element={<BasePage />}>
+              <Route index element={<Home />}></Route>
+              <Route path="favorite" element={<Favorites />}></Route>
+              <Route path=":id" element={<Player />}></Route>
+              <Route path="*" element={<NotFound />}></Route>
+            </Route>
           </Routes>
-        </FavoritesProvider>
-      </Container>
-
-      <Footer />
+        
     </BrowserRouter>
   );
 }
